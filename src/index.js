34b85c4efe2f8f8ef193f97b6f1a4e9aa28c4443
Adi7893 <amakwan9@my.centennialcollege.ca>
2024-08-04 18:38:33 -0400

    menuAdded

--- src/index.js
+++ src/index.js
@@ -70,7 +70,12 @@ function Menu(){
 return(
     <main className="menu">
         <h2>Our menu</h2>
-        <Pizza name="Pizza Spinachi" 
+        {pizzaData.map((pizza) => (
+         <Pizza pizzaObj={pizza} /> 
+        ))
+        }
+
+        {/*<Pizza name="Pizza Spinachi" 
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
         price={12} />
@@ -78,7 +83,7 @@ return(
         <Pizza name="Pizza Funghi" 
         ingredients="Tomato, mozarella, mushrooms, and onion"
         photoName="pizzas/funghi.jpg"
-        price={12} />
+price={12} />*/}
     </main>
     
 );
@@ -87,11 +92,11 @@ return(
 function Pizza(props) {
   return (
   <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-  <h3>{props.name}</h3>
-  <p>{props.ingredients}</p>
-  <span>{props.price}</span>
+  <h3>{props.pizzaObj.name}</h3>
+  <p>{props.pizzaObj.ingredients}</p>
+  <span>{props.pizzaObj.price}</span>
   </div>
   </div>
   );
